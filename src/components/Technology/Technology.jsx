7c216dd9c19f63bf080f94ca0b0bc7813c679f28
Kojo Akyer <kojoakyer777@gmail.com>
2022-01-26 14:08:16 +0000

    uppdate

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -12,8 +12,8 @@ function Technology() {
                 </div>
                 <div className="infoContainer">
                     <h1>About Me</h1>
-                    <h3>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</h3>
-                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                    <h3>My name is Kojo Akeyr, im a software developer who is dedicated to solving problems through software developement.</h3>
+                    <p>Software development has never been ‘just a job’ for me, it’s offered an engaging challenge to continually learn and improve my skill in creating high-quality software. What began with a simple ‘simple calculator in python’ has become a full-fledged passion that only gets more exciting as time goes by.</p>
                     <button className='techBtn'>Download cv</button>
                 </div>
             </div>
